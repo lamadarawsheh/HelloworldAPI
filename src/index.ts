3723fa886eb40aa0f84cf3e4ad5d1a44dd7aae66
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-09-02 14:43:29 +0300

    feat: adding endpoints -IMPL first endpoint : GET /hello?name={SOME_NAME} -IMPL secendpoint : GET /info

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,25 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
+app.set('trust proxy', true);
+
+app.get('/hello', (req: Request, res: Response) => {
+    
+        const name  = req.query.name as string;
+        const greeting = name ? `Hello,  ${name}`: `Hello, World!`;
+        res.json({greeting });
+  });
+  
+app.get('/info', (req: Request, res: Response) => {
+    
+        const request_time  = new Date().toISOString();
+        const host_name = os.hostname();
+        const client_address = req.ip;
+        const headers = req.headers;
+        
+        res.setHeader ('Content-Type',`application/json`);
+        res.send({request_time,client_address,host_name,headers });
+  });
 
 app.listen(port,()=>{
     console.log(`Server running at http://localhost:${port}/`);
